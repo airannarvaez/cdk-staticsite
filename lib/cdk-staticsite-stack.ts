8e49879d201503c641134171bf76cedca3c80337
airan narvaez <anarvae@bancolombia.com.co>
2024-01-17 16:20:52 -0300

    feat: add resources

--- lib/cdk-staticsite-stack.ts
+++ lib/cdk-staticsite-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
+import { Distribution } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { BlockPublicAccess, Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStaticsiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, 'my-bucket-id', {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      encryption: BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      versioned: true,
+      removalPolicy: RemovalPolicy.DESTROY
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStaticsiteQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const distribution = new Distribution(this, 'my-distribution-id', {
+      defaultBehavior: { origin: new S3Origin(bucket) },
+      defaultRootObject: 'index.html'
+    });
+
+    new CfnOutput(this, 'Distribution Domain', {
+      value: distribution.distributionDomainName
+    });
+
+    new CfnOutput(this, 'Bucket name', {
+      value: bucket.bucketName
+    });
   }
 }
